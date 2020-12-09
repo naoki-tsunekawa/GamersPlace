5999bd2e51b79132304193e05a057e9959bfb0b9
naoki-tsunekawa <b1014138@gmail.com>
2020-12-09 20:17:56 +0900

    update routes

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe "StaticPages", type: :system do
   describe "check title tags" do
     # titleタグのテスト
     it "the title contains home" do
-      visit "/static_pages/home"
+      visit root_path
       expect(page).to have_title 'GamersPlace'
     end
 
     it "the title contains help" do
-      visit "/static_pages/help"
+      visit help_path
       expect(page).to have_title 'Help | GamersPlace'
     end
 
     it "the title contains about" do
-      visit "/static_pages/about"
+      visit about_path
       expect(page).to have_title 'About | GamersPlace'
     end
 
     it "the title contains contact" do
-      visit "/static_pages/contact"
+      visit contact_path
       expect(page).to have_title 'Contact | GamersPlace'
     end
   end
