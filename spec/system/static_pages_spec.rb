8b23aad68e48cfb2b41fe1c7694e33d12baf34fb
naoki-tsunekawa <b1014138@gmail.com>
2020-12-09 23:44:20 +0900

    create user signup page

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :system do
 
+  # titleタグのテスト
   describe "check title tags" do
-    # titleタグのテスト
     it "the title contains home" do
       visit root_path
       expect(page).to have_title 'GamersPlace'
@@ -22,6 +22,19 @@ RSpec.describe "StaticPages", type: :system do
     it "the title contains contact" do
       visit contact_path
       expect(page).to have_title 'Contact | GamersPlace'
+    end
+  end
+
+  # リンクテスト
+  scenario "testing for layout links" do
+    visit root_path
+    aggregate_failures do
+      expect(page).to have_link 'Gamers Place', href: root_path
+      expect(page).to have_link 'Home', href: root_path
+      expect(page).to have_link 'Help', href: help_path
+      expect(page).to have_link 'About', href: about_path
+      expect(page).to have_link 'Contact', href: contact_path
+      # expect(page).to have_link 'Sign up now!', href: signup_path
     end
   end
 
