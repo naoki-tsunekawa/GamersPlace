ba0ff6484b0bba223c2f0d0bb2377f1630890bb2
naoki-tsunekawa <b1014138@gmail.com>
2020-12-19 21:50:56 +0900

    create messages style

--- spec/system/users_signups_spec.rb
+++ spec/system/users_signups_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSignups", type: :system do
+  # signup失敗時のmessageテスト
   scenario 'Don\'t create new data when user submits invalid information' do
     visit signup_path
     fill_in 'Name', with: ' '
@@ -12,6 +13,22 @@ RSpec.describe "UsersSignups", type: :system do
       expect(current_path).to eq users_path
       expect(page).to have_content 'Sign up'
       expect(page).to have_content 'The form contains 4 errors'
+    end
+  end
+
+  # signup成功時のflashテスト
+  scenario 'Create new data when user submits valid information' do
+    visit signup_path
+    fill_in 'Name', with: 'Example User'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirmation', with: 'password'
+    click_on 'Create my account'
+    aggregate_failures do
+      expect(current_path).to eq user_path(User.last)
+      expect(has_css?('.alert-success')).to be_truthy
+      visit current_path # <= 再読み込みを再現しています。
+      expect(has_css?('.alert-success')).to be_falsy
     end
   end
 end
