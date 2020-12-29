1d9e3b884506e63c92c5f5bc1883d787bd625da9
naoki-tsunekawa <b1014138@gmail.com>
2020-12-29 15:50:26 +0900

    Implement advanced login

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -35,4 +35,17 @@ RSpec.describe "Sessions", type: :request do
     end
   end
 
+  # remember meのテスト
+  describe 'remember me' do
+    it 'remembers the cookie when user checks the Remember Me box' do
+      log_in_as(user)
+      expect(cookies[:remember_token]).not_to eq nil
+    end
+
+    it 'does not remembers the cookie when user does not checks the Remember Me box' do
+      log_in_as(user, remember_me: '0')
+      expect(cookies[:remember_token]).to eq nil
+    end
+  end
+
 end
