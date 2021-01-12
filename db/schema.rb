513830fa76a2399a21643cd28436d449be0133c2
naoki-tsunekawa <b1014138@gmail.com>
2021-01-12 21:30:18 +0900

    create game model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_103220) do
+ActiveRecord::Schema.define(version: 2021_01_12_122752) do
+
+  create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "console"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
---
+++ spec/factories/games.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :game do
+    title { "MyString" }
+    console { "MyString" }
+  end
+end
