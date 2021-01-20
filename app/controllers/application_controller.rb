be2b0796f08b9917ad08671e7c9bd8fbad3b4092
naoki-tsunekawa <b1014138@gmail.com>
2021-01-20 20:21:26 +0900

    update games_request_spec.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,9 @@ class ApplicationController < ActionController::Base
 			redirect_to login_url
 		end
 	end
+
+	# 管理者かどうか確認
+	def admin_user
+		redirect_to(root_url) unless current_user.admin?
+	end
 end
