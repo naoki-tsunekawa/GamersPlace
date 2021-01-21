f93da4c80cf965dda51df9b0c6e9c8edbe927aa0
naoki-tsunekawa <b1014138@gmail.com>
2021-01-21 20:09:16 +0900

    create Game model spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:game) { FactoryBot.build(:game) }
+
+  # 有効な掲示板を作成できるかどうかのテスト
+  it 'it is a valid Game' do
+    expect(game).to be_valid
+  end
+
+  # バリデーションテスト
+  it 'is invalid with no title' do
+    game.title = ' '
+    expect(game).to be_invalid
+  end
+
 end
