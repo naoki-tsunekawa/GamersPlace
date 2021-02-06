072e7e6fdf7ec2f52feb6d718c2ba15b4cca5843
naoki-tsunekawa <b1014138@gmail.com>
2021-02-06 21:56:32 +0900

    create follow followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
 	has_many :posts, dependent: :destroy
 	has_many :games, through: :posts
+	has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
+	has_many :followers, through: :passive_relationships, source: :follower
 	attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 
@@ -38,5 +42,20 @@ class User < ApplicationRecord
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # バリデーションに関するテスト
+  describe "validation" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:other_user) { FactoryBot.create(:user) }
+    let(:relationship) { user.active_relationships.build(followed_id: other_user.id) }
+
+    it "is valid with test data" do
+      expect(relationship).to be_valid
+    end
+
+    describe "presence" do
+      it "is invalid without follower_id" do
+        relationship.follower_id = nil
+        expect(relationship).to be_invalid
+      end
+      it "is invalid without followed_id" do
+        relationship.followed_id = nil
+        expect(relationship).to be_invalid
+      end
+    end
+  end
+
+  # フォロー、フォロワー、フォロー解除に関するテスト
+  describe "follow and unfollow" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:other_user) { FactoryBot.create(:user) }
+
+    before { user.follow(other_user) }
+
+    describe "follow" do
+      it "succeeds" do
+        expect(user.following?(other_user)).to be_truthy
+      end
+    end
+
+    describe "followers" do
+      it "succeeds" do
+        expect(other_user.followers.include?(user)).to be_truthy
+      end
+    end
+
+    describe "unfollow" do
+      it "succeeds" do
+        user.unfollow(other_user)
+        expect(user.following?(other_user)).to be_falsy
+      end
+    end
+  end
 end
