5cf7c6e0f1a8ab24dad4591c54a50119150eb742
naoki-tsunekawa <b1014138@gmail.com>
2021-02-07 17:30:24 +0900

    create follow button

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # ルートページ
   root 'static_pages#home'
 
-  # user
   get '/help', to:'static_pages#help'
   get '/about', to:'static_pages#about'
   get '/contact', to:'static_pages#contact'
@@ -13,9 +12,19 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+
+  # user
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  # game
   resources :games
+  # post
   resources :posts, only: [:create, :destroy]
+  # relationship
+  resources :relationships, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# 管理者ユーザ作成
 User.create!(name:  "Example User",
 	email: "example@railstutorial.org",
 	password:              "foobar",
 	password_confirmation: "foobar",
 	admin: true)
 
-10.times do |n|
+#サンプルユーザの作成
+99.times do |n|
 name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
@@ -22,16 +24,23 @@ User.create!(name:  name,
 		password_confirmation: password)
 end
 
+# サンプルゲーム掲示板の作成
 Game.create!(title: "Sample Game Board",
 	description: "Sample description",
 	game_image: File.open("./public/images/600×400.png"))
 
+# サンプル投稿の作成
 user = User.first
 game = Game.first
-
 5.times do |i|
 	content = "sample content"
 	Post.create!(content: "sample content", user_id: user.id, game_id: game.id)
 end
 
-
+# フォロー、フォロワーのサンプルデータ作成
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
