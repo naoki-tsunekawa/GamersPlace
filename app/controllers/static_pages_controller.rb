b1e19775b6dbf49378fe43d75e1cb765774f2272
naoki-tsunekawa <b1014138@gmail.com>
2021-02-27 16:54:35 +0900

    update docker-compose.yml

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    @user = User.find(current_user.id)
+    if current_user != nil then
+      @user = User.find(current_user.id)
 
-    # ログイン中のユーザのお気に入りgame_idカラムを取得
-    myfavoritegame = Favoritegame.where(user_id: current_user.id).pluck(:game_id)
-    # gameテーブルからお気に入り登録ずみのレコードを取得
-    @favorite_list = Game.find(myfavoritegame)
+      # ログイン中のユーザのお気に入りgame_idカラムを取得
+      myfavoritegame = Favoritegame.where(user_id: current_user.id).pluck(:game_id)
+      # gameテーブルからお気に入り登録ずみのレコードを取得
+      @favorite_list = Game.find(myfavoritegame)
+    end
   end
 
   def help
