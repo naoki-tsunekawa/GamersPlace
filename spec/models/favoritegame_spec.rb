968d8bb39940269383f7f9e275e7915f62262307
naoki-tsunekawa <b1014138@gmail.com>
2021-04-24 22:25:55 +0900

    create_favoritegame_model_spec

--- spec/models/favoritegame_spec.rb
+++ spec/models/favoritegame_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Favoritegame, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:favoritegame) { FactoryBot.create(:favoritegame) }
+  let(:user) { FactoryBot.build(:user) }
+  let(:game) { FactoryBot.build(:game) }
+
+  it "is valid with favorite's test data" do
+    expect(favoritegame).to be_valid
+  end
+
+  # バリデーションのテスト
+  # game_id
+  it "is invalid with no game_id" do
+    favoritegame.game_id = nil
+    expect(favoritegame).to be_invalid
+  end
+
+  # user_id
+  it "is invalid with same user_id" do
+    favoritegame.user_id = nil
+    expect(favoritegame).to be_invalid
+  end
+
+  # 関連づけ削除テスト
+
+
 end
