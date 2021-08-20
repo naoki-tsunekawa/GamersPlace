13f0f80d9118c3683ad133f749db84cc494f741d
naoki-tsunekawa <b1014138@gmail.com>
2021-08-20 17:46:02 +0900

    update review page

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
   validates :score, presence: true
+
+  # ユーザー個人のレビューのメソッド定義
+  def user_review_score_percentage
+		unless self.score.nil?
+			score.round(1).to_f*100/5
+		else
+			0.0
+		end
+	end
 end
